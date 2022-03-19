34cb9b3c3419a924bec23b1080b22bbe6779f45d
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-03-19 13:07:43 -0700

    added 3 pokemon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert("Hello World");
-let favoriteFood = "Tacos";
-document.write(favoriteFood);
+let pokemonList = [{
+
+        name: 'Eevee',
+        height: 0.3,
+        type: ['normal']
+      },
+
+      {
+        name: 'Jigglypuff',
+        height: 0.5,
+        type: ['fairy', 'normal']
+      },
+
+      {
+        name: 'Cubone',
+        height: 0.4,
+        type: ['ground']
