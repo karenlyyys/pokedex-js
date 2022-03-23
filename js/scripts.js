4e0534e63f6c11c4106f7a1d0c383a6870655b32
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-03-23 14:32:33 -0700

    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 let pokemonList = [{
-        name: 'Eevee',
-        height: 0.3,
-        type: ['normal']
-      },
+  name: 'Eevee',
+  height: 0.3,
+  type: ['normal']
+},
 
-      {
-        name: 'Jigglypuff',
-        height: 0.5,
-        type: ['fairy', 'normal']
-      },
+{
+  name: 'Jigglypuff',
+  height: 0.5,
+  type: ['fairy', 'normal']
+},
 
-      {
-        name: 'Cubone',
-        height: 0.4,
-        type: ['ground']
-}];
+{
+  name: 'Cubone',
+  height: 0.4,
+  type: ['ground']
+}]
+
+// Loop to list all the pokemons on the page
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write("name: " + pokemonList[i].name + " (Height: " + pokemonList[i].height + ".");
+	document.write("type: " + pokemonList[i].type[0]);
+}
